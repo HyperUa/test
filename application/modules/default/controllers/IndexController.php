20b74634f80b5c78b96e218a4ca85372746912c8
Sergey Miroshnikov <hyperxai@gmail.com>
2014-02-18 18:47:27 +0200

    zend init

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -6,12 +6,28 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $registry = Zend_Registry::getInstance();
+        $this->_em = $registry->entitymanager;
     }
 
     public function indexAction()
     {
         // action body
         //d('Hello = indexAction');
+
+        $book = $this->_em->getRepository('Model_Book')->findOneBy(array(
+            'id' => 2
+        ));
+
+        // d($book->getId());
+
+
+        $testEntity = new Model_Book;
+        $testEntity->setName('Some name');
+        $this->_em->persist($testEntity);
+        $this->_em->flush();
+
+
         $this->view->hello = 'Hello Smarty';
     }
 
---
+++ library/Site/Controller/ActionController.php
@@ -0,0 +1,7 @@
+<php
+
+    class Site_Controller_Action extends Zend_Controller_Action{
+
+
+}
+
